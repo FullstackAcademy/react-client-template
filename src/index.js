12ea196b615816f349420756cfb883dadc816438
ericpkatz <ericpkatz@gmail.com>
2022-12-14 07:36:36 -0500

    fix template

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import React, { useState, useEffect } from 'react';
 import { HashRouter, Routes, Route, Link} from 'react-router-dom';
 
@@ -6,7 +6,7 @@ import { HashRouter, Routes, Route, Link} from 'react-router-dom';
 const App = ()=> {
   return (
     <div>
-      <h1>React Template</h1>
+      <h1>React Client Template</h1>
       <nav>
         <Link to='/'>Home</Link>
       </nav>
